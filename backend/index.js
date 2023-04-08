d3fb21f6fb32a2cfd4302ed796b6764573736668
Maz5710 <marian.arnot@gmail.com>
2023-04-08 13:01:01 +1200

    added mongodb api to backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -21,28 +21,105 @@ app.use((req, res, next) => {
 // PLUGGING CORS INTO EXPRESS
 app.use(cors());
 
-app.get('/', (req, res) => res.send("Hello from the backend"));// Sent to backend on req
+app.get('/', (req, res) => res.send("Hello from the backend")); // Sent to backend on req
+
+/// *** Maz added 7 April start *** ///
+app.use(bodyParser.json()); // calling Body Parser method and urlencoded 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(cors()); // calling cors method with express
+/// *** Maz added 7 April end *** ///
 
 // Setup Mongoose Connection to MongoDB
 mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net/${config.MONGO_DBNAME}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('DB Connected'))
-.catch(err => {
-    console.log(`DB Connection Error: ${err.message}`);
-})
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('DB Connected'))
+    .catch(err => {
+        console.log(`DB Connection Error: ${err.message}`);
+    })
 
-app.listen(port, () => console.log(`My fullstack app is listening on port ${port}`))// sent to nodemon - checking server
+app.listen(port, () => console.log(`My fullstack app is listening on port ${port}`)) // sent to nodemon - checking server
 
-// Get All Products from the Database
+// Get All Products from the Database 
 app.get('/allProductsFromDB', (req, res) => {
     Product.find().then(result => {
         res.send(result)
     })
 })
 
+/// *** Maz added 7 April start *** ///
 // ------ PRODUCT END POINTS START -------------
+
+// Post Method to CREATE a Product Start
+app.post('/addProduct', (req, res) => {
+    const dbProduct = new Product({
+        _id: new mongoose.Types.ObjectId,
+        productName: req.body.productName,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        condition: req.body.condition,
+        image1: req.body.image1,
+        image2: req.body.image2,
+        image3: req.body.image3,
+        image4: req.body.image4,
+        user_id: req.body.user_id
+    });
+    //save to the database and notify the user
+    dbProduct.save().then(result => {
+        res.send(result);
+    }).catch(err => res.send(err));
+})
+// End of CREATE End Point
+
+// Edit UPDATE using 'PATCH' http method
+app.patch('/updateProduct/:id', (req, res) => {
+    const idParam = req.params.id;
+    Product.findById(idParam, (err, product) => {
+        const updatedProduct = {
+            productName: req.body.productName,
+            description: req.body.description,
+            price: req.body.price,
+            category: req.body.category,
+            condition: req.body.condition,
+            image1: req.body.image1,
+            image2: req.body.image2,
+            image3: req.body.image3,
+            image4: req.body.image4,
+        }
+        Product.updateOne({
+            _id: idParam
+        }, updatedProduct).
+        then(result => {
+            res.send(result);
+        }).catch(err => res.send(err))
+    })
+})
+// End of UPDATE End Point
+
+// DELETE using 'DELETE' http Method Start 
+app.delete('/deleteProduct/:id', (req, res) => {
+    const idParam = req.params.id;
+    Product.findOne({
+        _id: idParam
+    }, (err, product) => {
+        if (product) {
+            Product.deleteOne({
+                _id: idParam
+            }, err => {
+                console.log('deleted on backend request');
+            });
+        } else {
+            alert('not found');
+        }
+    }).catch(err => res.send(err));
+});
+// End of DELETE End Point
 // ------ PRODUCT END POINTS END ---------------
+/// *** Maz added 7 April end *** ///
 
 
 
